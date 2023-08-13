9615ffec451312bb34a7927edfea2bca2f66c654
yash somkuwar <somkuwar.techfest@gmail.com>
2023-08-13 21:24:58 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Define routes after the static middleware
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('update-artwork', (data) => {
+        io.emit('update-artwork', data);  // Broadcasting the artwork update to all clients.
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
